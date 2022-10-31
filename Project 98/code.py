5947347b61bbb1d915dfae8332b95733d2328838
bak <59486508+bakerrrrr@users.noreply.github.com>
2022-10-31 23:56:02 +0000

    Create code.py
    Learning python.

---
+++ Project 98/code.py
@@ -0,0 +1,32 @@
+# age = 3;
+
+# if (age <= 12):
+#        print('User is a kid.')
+# elif (age >= 13 and age <= 17):
+#        print('User is a teenager.')
+# elif (age >= 18):
+#        print('User is an adult.');
+
+# planets = ['Earth', 'Mars', 'Venus', 'Saturn', 'Jupiter', 'Uranus', 'Neptune', 'Mercury']
+
+# for p in planets:
+#        print(p);
+
+# for i in range(0, len(planets)):
+#        print(i);
+
+# for i in range(1, 101):
+#        if (i % 2 == 0):
+#               print(i);
+
+# for i in range(0, 101, 5):
+#        print(i);
+
+count = 5;
+while (count > 0):
+       print(count);
+       count = count - 1;
+
+
+# print(planets[3])
+# print(len(planets))
