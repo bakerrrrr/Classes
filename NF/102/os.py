7a2cfcf5d0cce191a81125bba6dc06b8ebec8a91
bak <59486508+bakerrrrr@users.noreply.github.com>
2022-11-29 00:55:18 +0000

    Create os.py

---
+++ NF/102/os.py
@@ -0,0 +1,12 @@
+# import os
+
+# baseDir = os.getcwd()
+
+# print(os.getcwd()) # CWD stands for [Currently Working Directory]
+# os.mkdir(baseDir + '/Proof')
+
+# print(os.listdir('C:/Users/?/Documents/'))
+# print(os.path.exists(baseDir + '/Proof'))
+
+# root, ext = os.path.splitext(baseDir + '/User')
+# print(root, ext)
